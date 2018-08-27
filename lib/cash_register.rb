646bc2b0107c05e22f4b3aa6f630c14603852ede
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-27 02:51:26 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ def add_item(title, price, quantity = 1)
 
 end
 
-def apply_discount
+def apply_discount(discount)
 final_price = @total - @discount
 if @discount != 0
   puts "After the discount, the total comes to #{final_price}."
